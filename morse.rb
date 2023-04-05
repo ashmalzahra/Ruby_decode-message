8d0758fff2b9ad2d0e454c1ef27f665046412e0f
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-06 00:41:55 +0500

    Decode morse char and word

---
+++ morse.rb
@@ -0,0 +1,22 @@
+def decode_char(character)
+    morse_dictionary = {
+        '.-' => 'A',  '-...' => 'B',  '-.-.' => 'C',
+        '..-.' => 'F',  '.' => 'E',  '-..' => 'D',
+        '--.' => 'G',  '....' => 'H',  '..' => 'I', 
+        '.-..' => 'L',  '-.-' => 'K',  '.---' => 'J',
+        '--' => 'M',  '-.' => 'N',  '---' => 'O',  
+        '.-.' => 'R',  '--.-' => 'Q',  '.--.' => 'P',
+        '...' => 'S',  '-' => 'T',  '..-' => 'U', 
+        '-..-' => 'X',  '.--' => 'W',  '...-' => 'V',
+        '-.--' => 'Y',  '--..' => 'Z'
+    }
+    morse_dictionary[character] || ' '
+end
+
+def decode_word(morse_word)
+    word = ''
+    morse_word.split.each do |char|
+        word += decode_char(char)
+    end
+    word
+end
